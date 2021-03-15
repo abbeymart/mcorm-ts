b4f464c0cfda200ea8343884dc095b78fffca652
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2021-03-15 12:18:27 -0400

    updated dependencies

--- src/helpers/jsonToCrudGetParams.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @Author: abbeymart | Abi Akindele | @Created: 2020-08-01
- * @Company: Copyright 2020 Abi Akindele  | mConnect.biz
- * @License: All Rights Reserved | LICENSE.md
- * @Description: mc-central-ts: client-json-request to crudGetParams
- */
